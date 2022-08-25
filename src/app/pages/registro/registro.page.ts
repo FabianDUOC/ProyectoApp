ab294d9fda316908e1b678634ce217ded9e7ae97
GatoMamado <lu.morgado@duocuc.cl>
2022-08-25 12:53:43 -0400

    1.5
    -Cambio de color a gradiente en Botones grises
    -Validaciones básicas para el registro

--- src/app/pages/registro/registro.page.ts
+++ src/app/pages/registro/registro.page.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { NavigationExtras, Router } from '@angular/router';
+import { AlertController } from '@ionic/angular';
 
 @Component({
   selector: 'app-registro',
@@ -7,9 +9,57 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RegistroPage implements OnInit {
 
-  constructor() { }
+  correo: string = "";
+  nombre: string = "";
+  apellido: string = "";
+  clave: string = "";
+  clave2: string = "";
 
-  ngOnInit() {
+  constructor(private router: Router, private alertController: AlertController) { }
+
+  async alertContraseña() {
+    const alert = await this.alertController.create({
+      message: 'Las contraseñas deben tener al menos 8 caracteres y deben ser iguales',      
+    });
+  
+
+    await alert.present();
+  }
+  async alertCorreo() {
+    const alert = await this.alertController.create({
+      message: 'Ingrese un Correo Valido',      
+    });
+  
+
+    await alert.present();
+  }
+
+  async alertReg() {
+    const alert = await this.alertController.create({
+      message: 'Cuenta Registrada Correctamente',      
+    });
+  
+
+    await alert.present();
+  }
+
+  verifRegistrar(){
+
+    if (this.clave.length <= 8 || this.clave != this.clave2){
+      this.alertContraseña();
+      
+      }
+    else if (this.correo.includes("@") ==false || this.correo.includes(".") ==false){
+      this.alertCorreo();
+        
+      }
+    else{
+      this.alertReg();
+      this.router.navigate(['/inicio']);
+    }
+  }
+  ngOnInit() {   
   }
 
 }
+
