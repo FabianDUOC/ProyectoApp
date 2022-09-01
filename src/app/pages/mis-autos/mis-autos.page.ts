52fd9cd1fb5865937079f3705d41760bc525ec4d
FabianDUOC <fa.marileo@duocuc.cl>
2022-09-01 13:01:06 -0400

    1.32
    - Actualización en mis-autos

--- src/app/pages/mis-autos/mis-autos.page.ts
+++ src/app/pages/mis-autos/mis-autos.page.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { NavigationExtras, Router } from '@angular/router';
+import { AlertController } from '@ionic/angular';
 
 @Component({
   selector: 'app-mis-autos',
@@ -8,7 +9,16 @@ import { NavigationExtras, Router } from '@angular/router';
 })
 export class MisAutosPage implements OnInit {
 
-  constructor(private router: Router) { }
+  autos:any = [
+    {
+      patente:"xyxy-98",
+      marca:"Hyundai",
+      modelo:"Ioniq",
+      imagen:"/assets/ioniq.jpg", 
+    }
+  ]
+
+  constructor(private router: Router, private alertController: AlertController) { }
 
   // Seleccion del Menu Footer
   segmentChanged($event) {
@@ -20,6 +30,18 @@ export class MisAutosPage implements OnInit {
       }
     }
     this.router.navigate(['menu/' + direccion],navigationExtras);
+  }
+
+  eliminarAuto(){
+    this.autos = null;
+    this.alertEli();
+  }
+
+  async alertEli() {
+    const alert = await this.alertController.create({
+      message: 'Auto eliminado <br> correctamente',      
+    });
+    await alert.present();
   }
 
   ngOnInit() {
