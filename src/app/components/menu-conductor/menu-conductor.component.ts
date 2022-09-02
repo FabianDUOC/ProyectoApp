11f79f97549d4f595e32cb4ebd97872967a6e822
FabianDUOC <fa.marileo@duocuc.cl>
2022-09-01 23:17:37 -0400

    1.35
    - Agregada sección de ver/cancelar viaje para conductor.

--- src/app/components/menu-conductor/menu-conductor.component.ts
+++ src/app/components/menu-conductor/menu-conductor.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute, Router } from '@angular/router';
+import { ActivatedRoute, NavigationExtras, Router } from '@angular/router';
 
 @Component({
   selector: 'app-menu-conductor',
@@ -10,12 +10,38 @@ export class MenuConductorComponent implements OnInit {
 
   usuario:any;
 
+  //Listas para Ver/Cancelar Viaje
+  conductor:string = "Pedro Gomez";
+  conductorImagen:any = "../../../assets/perfil2.jpg";
+
+  pasajeros:any = [
+    {
+      nombre:"Guillermo Astudillo",
+      imagen:"../../../assets/profile_.png",
+    },
+    {
+      nombre:"Juan Pérez",
+      imagen:"../../../assets/perfilEjemplo.jpg", 
+    },
+  ]
+
   constructor(private router: Router, private activedRouter: ActivatedRoute) { 
     this.activedRouter.queryParams.subscribe(params => {
       if (this.router.getCurrentNavigation().extras.state) {
         this.usuario = this.router.getCurrentNavigation().extras.state.u;
       }
     })
+  }
+
+  verViajeC(){
+    let navigationExtras: NavigationExtras = {
+      state:{
+        c: this.conductor,
+        ic: this.conductorImagen,
+        p: this.pasajeros,
+      }
+    }
+    this.router.navigate(['/viaje-conductor'], navigationExtras);
   }
 
   ngOnInit() {}
--- src/app/pages/viaje-conductor/viaje-conductor.page.ts
+++ src/app/pages/viaje-conductor/viaje-conductor.page.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
-import { NavigationExtras, Router } from '@angular/router';
+import { ActivatedRoute, NavigationExtras, Router } from '@angular/router';
+import { AlertController } from '@ionic/angular';
+import { Location } from '@angular/common'
 
 @Component({
   selector: 'app-viaje-conductor',
@@ -8,7 +10,23 @@ import { NavigationExtras, Router } from '@angular/router';
 })
 export class ViajeConductorPage implements OnInit {
 
-  constructor(private router: Router) { }
+  conductor:string = "";
+  imagenConductor:any;
+  pasajeros:any;
+
+  hora:string = "15:40"
+
+  destino:string = "Recoleta 1107";
+
+  constructor(private router: Router, private activedRouter: ActivatedRoute, private alertController: AlertController,private location: Location) { 
+    this.activedRouter.queryParams.subscribe(params => {
+      if (this.router.getCurrentNavigation().extras.state) {
+        this.conductor = this.router.getCurrentNavigation().extras.state.c;
+        this.imagenConductor = this.router.getCurrentNavigation().extras.state.ic;
+        this.pasajeros = this.router.getCurrentNavigation().extras.state.p;
+      }
+    })
+  }
 
   // Seleccion del Menu Footer
   segmentChanged($event) {
@@ -22,6 +40,25 @@ export class ViajeConductorPage implements OnInit {
     this.router.navigate(['menu/' + direccion],navigationExtras);
   }
 
+  cancelarV(){
+    this.conductor = "";
+    this.pasajeros = null;
+    
+    this.hora = ""
+    this.destino= "";
+
+    this.alert();
+  }
+
+  async alert() {
+    const alert = await this.alertController.create({
+      message: 'Viaje cancelado',      
+    });
+    await alert.present();
+  }
+  back(): void {
+    this.location.back()
+  }
   ngOnInit() {
   }
 
